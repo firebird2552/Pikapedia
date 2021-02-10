49f0e53887fc63d1910227bd3021efe0e68c1358
firebird2552 <firebird2552@gmail.com>
2021-02-10 13:45:35 -0700

    Add link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
 
 // react-bootstrap imports
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 // custom imports
 
@@ -19,6 +20,9 @@ const Header = () => {
                 <Link to='/'>
                     <img src={Logo} alt="Pikapedia" className="mw-100 h-auto" width="315" height="100" />
                 </Link>
+                <Nav>
+                    <Link to="Version1">Red/Blue</Link>
+                </Nav>
             </Navbar.Brand>
         </Navbar>
     )
